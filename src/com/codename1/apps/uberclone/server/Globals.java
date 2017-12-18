3610fc395521f6695e4d154b26284edf1d3040e2
Shai Almog <shai@codenameone.com>
2017-12-18 22:58:02 +0200

    Updated webservice URL

--- src/com/codename1/apps/uberclone/server/Globals.java
+++ src/com/codename1/apps/uberclone/server/Globals.java
@@ -30,5 +30,5 @@ package com.codename1.apps.uberclone.server;
  */
 public class Globals {
     public static final String SERVER_URL = "http://localhost:8080/";
-    public static final String SERVER_SOCKET_URL = "http://localhost:8080/wsMsg";
+    public static final String SERVER_SOCKET_URL = "ws://localhost:8080/wsMsg";
 }
